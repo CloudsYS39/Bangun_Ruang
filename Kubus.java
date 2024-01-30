a602a0eb96d01129ad56bae0a005bfe8dbcb173e
CloudsYS39 <156266176+CloudsYS39@users.noreply.github.com>
2024-01-30 09:33:32 +0700

    Add files via upload

---
+++ Kubus.java
@@ -0,0 +1,17 @@
+public class Kubus extends BangunRuang{
+    float s;
+
+    @Override
+float volume(){
+   float volume = s * s * s;
+   System.out.println("Volume kubus: " + volume);
+   return volume;
+}
+
+    @Override
+float lp(){
+   float lp = 6 * s * s;
+   System.out.println("Luas Permukaan kubus: " + lp);
+   return lp;
+}
+}
